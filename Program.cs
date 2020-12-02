5059fbf7c918b49ddc660e0298d5af945517cf98
Danijel Drazetic <daddo.1009@gmail.com>
2020-12-02 19:01:34 +0100

    dodano

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace appp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pozdrav od Danijela Drazetica!");
+        }
+    }
+}
